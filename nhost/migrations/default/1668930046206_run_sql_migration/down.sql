d28c8c29786d007a76b13e2963d013eca8ed8f52
Johan Eliasson <johan@eliasson.me>
2022-11-21 11:32:49 +0100

    init

---
+++ nhost/migrations/default/1668930046206_run_sql_migration/down.sql
@@ -0,0 +1,20 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- CREATE OR REPLACE FUNCTION sync_files() RETURNS TRIGGER AS
+--
+-- $BODY$
+-- BEGIN
+--     INSERT INTO
+--         public.public_files(file_id)
+--         VALUES(new.id);
+--         RETURN new;
+-- END;
+-- $BODY$
+-- language plpgsql;
+--
+-- DROP TRIGGER IF EXISTS trig_sync_student_files ON storage.files;
+-- CREATE TRIGGER trig_sync_student_files
+--      AFTER INSERT ON storage.files
+--      FOR EACH ROW
+--      WHEN (new.bucket_id = 'default')
+--      EXECUTE PROCEDURE sync_files();
